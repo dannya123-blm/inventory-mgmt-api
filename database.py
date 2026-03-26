9f9d5f55f7084ed5d6d70c0410600b98d83e4dd8
Daniel Aigbe <146071073+dannya123-blm@users.noreply.github.com>
2026-03-26 08:34:36 +0000

    change hardcode db connection to .env for later access

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
+import os
 from pymongo import MongoClient
-MONGO_URL = "mongodb+srv://admin11:adminpass11@inventory-management-db.ue0pccy.mongodb.net/inventoryDB?retryWrites=true&w=majority"
+MONGO_URL = os.getenv(
+    "MONGO_URL",
+    "mongodb+srv://admin11:adminpass11@inventory-management-db.ue0pccy.mongodb.net/inventoryDB?retryWrites=true&w=majority"
+)
 client = MongoClient(MONGO_URL)
 db = client["inventoryDB"]
 products_collection = db["products"]
--- load_csv.py
+++ load_csv.py
@@ -1,10 +1,19 @@
+import os
 import pandas as pd
 from pymongo import MongoClient
-MONGO_URL = "mongodb+srv://admin11:adminpass11@inventory-management-db.ue0pccy.mongodb.net/inventoryDB?retryWrites=true&w=majority"
+
+MONGO_URL = os.getenv(
+    "MONGO_URL",
+    "mongodb+srv://admin11:adminpass11@inventory-management-db.ue0pccy.mongodb.net/inventoryDB?retryWrites=true&w=majority"
+)
+
 client = MongoClient(MONGO_URL)
 db = client["inventoryDB"]
 collection = db["products"]
+
 df = pd.read_csv("products.csv")
 data = df.to_dict(orient="records")
-collection.insert_many(data)
+
+if data:
+    collection.insert_many(data)
 print("CSV uploaded successfully")
